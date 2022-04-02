ed90c0da9ee8668ed68a7c0c5553e2084790b253
Keith Elliott <kelliott@hilcorp.com>
2022-04-02 15:03:40 -0800

    Refine for MVP deployment

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,22 +1,37 @@
 import { useAuth } from '@redwoodjs/auth'
-import { navigate, routes } from '@redwoodjs/router'
 import usernameFromUserObject from 'src/util/usernameFromUserObject'
-// import { Link, routes } from '@redwoodjs/router'
-// import { MetaTags } from '@redwoodjs/web'
 import Flashcard from 'src/components/Flashcard/Flashcard'
+import FlashcardBox from 'src/components/Flashcard/FlashcardBox/FlashcardBox'
+import FlashcardTopicsCell from 'src/components/Flashcard/FlashcardTopicsCell'
 import defaultFlashcards from 'src/util/defaultFlashcards'
+import { Text, VStack, Link as ChakraLink } from '@chakra-ui/react'
 
 const HomePage = () => {
   const { isAuthenticated, currentUser } = useAuth()
 
-  if (isAuthenticated)
-    navigate(
-      routes.flashcardUser({ username: usernameFromUserObject(currentUser) })
-    )
-
-  return (
+  return isAuthenticated ? (
+    <FlashcardBox>
+      <VStack margin="auto">
+        {/* <Text fontSize="xl" color="gray.600">
+          Welcome {currentUser?.email}
+        </Text> */}
+        <Text fontSize="xl" color="blue.600">
+          <ChakraLink
+            color="blue.600"
+            href={`/new-topic/${usernameFromUserObject(currentUser)}`}
+          >
+            Create New Flashcard or Topic
+          </ChakraLink>
+        </Text>
+        <Text fontSize="xl" color="gray.600">
+          Topics by {usernameFromUserObject(currentUser)}:{' '}
+        </Text>
+        <FlashcardTopicsCell username={usernameFromUserObject(currentUser)} />
+      </VStack>
+    </FlashcardBox>
+  ) : (
     <Flashcard
-      username={'FlipAgain'}
+      username={'HelloFlipAgain'}
       topic={'Welcome'}
       order={1}
       flashcard={defaultFlashcards[0]}
